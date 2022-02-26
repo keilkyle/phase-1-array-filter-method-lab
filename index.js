8b01cd3fc6c53c0843ed378be47d2e99da07ebf4
Christian Keil <christian@astranis.com>
2022-02-25 23:19:48 -0800

    commit tree 816ad8d13540cca55b4807a8231929248df2d391

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+array = ["bob","sam"]
+
+function findMatching (array, name) {
+    return array.filter(function (x) {
+        return x.toUpperCase() === name.toUpperCase(); 
+    })
+}
+
+function fuzzyMatch (array, string) {
+    return array.filter(function (x) {
+        return string === x.substring(0, string.length)
+    })
+}
+
+function matchName (array, string) {
+    return array.filter (function (x) {
+        return x.name === string;
+    })
+}
